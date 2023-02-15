ce490c3a1556ef5e28d88a0b0e86bc57d6ce4c21
akshaykalhane <akshaykalhane1999@gmail.com>
2023-02-15 16:13:48 +0530

    complete code

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ async function getUser(){
     console.log(data)
     if(data.message){
         console.log('not found')
+        userNotFound()
     } else{
         displayUser(data)
     }
@@ -33,21 +34,61 @@ async function getUser(){
 
 function displayUser(user){
     if(user){
+        const twitter = user.twitter_username==null ? 'Twitter Not Available' : user.twitter_username
+        const blog= user.blog=='' ? 'No Blog' : user.blog
+        const company = user.company==null ? 'No Company' : user.company
         displayUI.innerHTML=`
-        <img src=${user.avatar_url} class="avatar" />
-        <p>@${user.login}</p>
-        <p>${user.name}</p>
-        <p>${user.bio}</p>
-        
+        <div class="profile-head">
+        <img src=${user.avatar_url} class="profile-img" />
+        <div class="info">
+        <h2>${user.name}</h2>
+        <p class="username">@${user.login}</p>
+        <p class="bio">${user.bio}</p>
+        </div>
+        <p class="join-date">Joined ${user.created_at.slice(0,10).split('-').join(' ')}</p>
+        </div>
+        <div class="profile-stat">
+            <div class="stat-box">
+                <p>Repos</p>
+                <h3>${user.public_repos}</h3>
+            </div>
+            <div class="stat-box">
+                <p>Followers</p>
+                <h3>${user.followers}</h3>
+            </div>
+            <div class="stat-box">
+                <p>Following</p>
+                <h3>${user.following}</h3>
+            </div>
+        </div>
+        <div class="more-info">
+            <div class="more-info-box">
+                <p>${user.location}</p>
+            </div>
+            <div class="more-info-box">
+                <p>${twitter}</p>
+            </div>
+            <div class="more-info-box">
+                <p>${blog}</p>
+            </div>
+            <div class="more-info-box">
+                <p>${company}</p>
+            </div>
+        </div>
        `
     }
 }
 
-function userNotFound(user){
-
+function userNotFound(){
+    // error.textContent='user not found'
+    displayUI.innerHTML=`
+    <h2 class="welcome-msg">user not found</h2>
+    `
 }
 
 function validate(){
     console.log('enter username')
     error.textContent='Enter Username First'
 }
+
+
